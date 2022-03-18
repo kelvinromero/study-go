54109e4af5b77a60503035c06fef033fb0682db4
Kelvin Romero <kelvin.romero@gmail.com>
2022-03-18 10:34:49 -0300

    iteration and arrays

---
+++ integers/adder.go
@@ -0,0 +1,9 @@
+package integers
+
+// When you have more than one argument of the same type
+// you can shorten the declaration to have the type once
+// after the last parameter
+func Add(x, y int) (result int) {
+	result = x + y
+	return
+}
