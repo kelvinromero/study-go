c3aa3d2032c6901af0be3ca8fa299fd0486691c5
Kelvin Romero <kelvin.romero@gmail.com>
2022-03-31 16:28:59 -0300

    Slices

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,8 +1,29 @@
 package arrays
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers)) // Create a new slice and append the sum of the numbers to it
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
 	return
 }
